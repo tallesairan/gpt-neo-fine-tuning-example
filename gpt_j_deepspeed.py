47d207d0e46e5baf9666408e5f813b30c8c23cff
deepschneider <komesergey@gmail.com>
2022-01-07 01:32:36 +0300

    Updated GPT-J-6B

--- gpt_j_deepspeed.py
+++ gpt_j_deepspeed.py
@@ -8,10 +8,13 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 import pandas as pd
 import torch
 from torch.utils.data import Dataset, random_split
-from transformers import AutoTokenizer, TrainingArguments, Trainer, AutoModelForCausalLM
+from transformers import AutoTokenizer, TrainingArguments, Trainer, AutoModelForCausalLM, IntervalStrategy
 
 torch.manual_seed(42)
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B", bos_token='<|endoftext|>', eos_token='<|endoftext|>', pad_token='<|pad|>')
+training_args = TrainingArguments(output_dir='./results', num_train_epochs=5, logging_steps=100, save_strategy=IntervalStrategy.NO,
+                                  per_device_train_batch_size=15, per_device_eval_batch_size=15, warmup_steps=100,
+                                  weight_decay=0.01, logging_dir='./logs', fp16=True, deepspeed='./ds_config_gpt_j.json')
 model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B").cuda()
 model.resize_token_embeddings(len(tokenizer))
 descriptions = pd.read_csv('netflix_titles.csv')['description']
@@ -39,9 +42,6 @@ class NetflixDataset(Dataset):
 dataset = NetflixDataset(descriptions, tokenizer, max_length=max_length)
 train_size = int(0.9 * len(dataset))
 train_dataset, val_dataset = random_split(dataset, [train_size, len(dataset) - train_size])
-training_args = TrainingArguments(output_dir='./results', num_train_epochs=5, logging_steps=300, save_steps=300,
-                                  per_device_train_batch_size=15, per_device_eval_batch_size=15,warmup_steps=100,
-                                  weight_decay=0.01, logging_dir='./logs', fp16=True, deepspeed='./ds_config_gpt_j.json')
 Trainer(model=model, args=training_args, train_dataset=train_dataset,
         eval_dataset=val_dataset, data_collator=lambda data: {'input_ids': torch.stack([f[0] for f in data]),
                                                               'attention_mask': torch.stack([f[1] for f in data]),
